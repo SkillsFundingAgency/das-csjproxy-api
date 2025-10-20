a0f6c408052630d280f73daf4c69aa3027a637f8
Tyler McQueen <tyler.mcqueen@education.gov.uk>
2025-10-20 16:27:13 +0100

    Added /src folder + source code from branch FAI-2823

---
+++ src/SFA.DAS.FAA.CSJProxy.Api/AppStart/AddServiceRegistrationExtension.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.FAA.CSJProxy.Api.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddServiceRegistrationExtension
+{
+    public static void AddApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        // validators
+        services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+    }
+
+    public static void ConfigureHealthChecks(this IServiceCollection services)
+    {
+        // health checks
+        services
+            .AddHealthChecks()
+            .AddCheck<DefaultHealthCheck>("default");
+    }
+}
